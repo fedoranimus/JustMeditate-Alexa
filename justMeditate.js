199ee100eec1deaab168f332a49c98db49233d3a
Tim Turner <timdturner@gmail.com>
2016-03-04 19:03:16 -0500

    Added justMeditate core

---
+++ justMeditate.js
@@ -0,0 +1,20 @@
+'use strict'
+var AlexaSkill = require('./AlexaSkill'),
+    eventHandlers = require('./eventHandlers'),
+    intentHandlers = require('./intentHandlers');
+    
+var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var skillContext = {};
+
+var JustMeditate = function() {
+    AlexaSkill.call(this, APP_ID);
+    skillContext.needMoreHelp = true; //what does this do?
+}
+
+JustMeditate.prototype = Object.create(AlexaSkill.prototype);
+JustMeditate.prototype.constructor = JustMeditate;
+
+eventHandlers.register(JustMeditate.prototype.eventHandlers, skillContext);
+intentHandlers.register(JustMeditate.prototype.intentHandlers, skillContext);
+
+module.exports = JustMeditate;
